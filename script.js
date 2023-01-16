d58a9b05c801bb2154fae3c7e6dfe761a6afa1d6
Dinesh kumar <dinneshkumar9@gmail.com>
2023-01-16 20:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-function countWord() {
-          
-    // Get the input text value
-    var words = document
-        .getElementById("evaluatedText").value;
-  
-    // Initialize the word counter
-    var count = 0;
-  
-    // Split the words on each
-    // space character 
-    var split = words.split(' ');
-  
-    // Loop through the words and 
-    // increase the counter when 
-    // each split word is not empty
-    for (var i = 0; i < split.length; i++) {
-        if (split[i] != "") {
-            count += 1;
-        }
-		else{
-			count += 1;
-		}
-    }
-  
-    // Display it as output
-    document.getElementById("letterCount")
-        .innerHTML = count;
-}
+const textbox = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
+
+	textbox.addEventListener('input', function () {
+		letterCount.innerHTML = textbox.value.length;
+	})
